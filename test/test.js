ef1cb54bc1ea641c6051d1e1d6bc02962c6c3bff
Heather Tran <heatherhttran@gmail.com>
2016-09-28 16:12:15 -0400

    Changing test output.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-	it('respond with hello world', function(done) {
+	it('give me a successful build!', function(done) {
 		request(app).get('/').expect('hello world', done);
 	});
 });
