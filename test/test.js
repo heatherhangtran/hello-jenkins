bd326ffd75fbb00653810422cdf4b6235f5a0f6d
Heather Tran <heatherhttran@gmail.com>
2016-09-29 20:00:07 -0400

    Testing CI.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
 	it('give me a successful build!', function(done) {
-		request(app).get('/').expect('hello world', done);
+		request(app).get('/').expect('hello jenkins again', done);
 	});
 });
